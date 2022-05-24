1ace642b6338b7959733f50635fc3bae04b3e784
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-05-24 13:04:21 +0200

    shouldHaveFewBombWhenGenerated & shouldHaveAllBombsWhenFull -> passed

--- src/main/java/campus/valence/demineur/Case.java
+++ src/main/java/campus/valence/demineur/Case.java
@@ -1,5 +1,7 @@
 package campus.valence.demineur;
 
+import java.util.Objects;
+
 public class Case {
 
     static class BombCase extends Case {
@@ -18,8 +20,35 @@ public class Case {
     private int x;
     private int y;
 
-    public Case(int x, int y) {
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    protected Case(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Case aCase = (Case) o;
+        //
+        return x == aCase.x && y == aCase.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
--- src/main/java/campus/valence/demineur/Demineur.java
+++ src/main/java/campus/valence/demineur/Demineur.java
@@ -1,9 +1,34 @@
 package campus.valence.demineur;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import campus.valence.demineur.Case.*;
+
+
 public class Demineur {
 
     public static Demineur create(int bombCount, int boardSize) {
         Case[][] board = new Case[boardSize][boardSize];
+
+        // set de bombes, aux positions aléatoires,
+        Set<Case>bombSet = new HashSet<>();
+
+        // Tant que le set n'a pas atteint la longueur voulue (bombcount), j'ajoute une instance de case axu coordonnées aléatoires
+        while(bombSet.size()<bombCount){
+            int x = (int) ((Math.random()*(boardSize)));
+            int y = (int) ((Math.random()*(boardSize)));
+            bombSet.add( new BombCase(x,y));
+        }
+
+
+        // mettre les bombes sur la board
+        for (Case bomb:bombSet) {
+            board[bomb.getX()][bomb.getY()]=bomb;
+        }
+
+        System.out.println("bombSet -> " + bombSet);
+
         return new Demineur(board);
     }
 
